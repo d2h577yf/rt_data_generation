6c272a265aa5cb54f37c7c508212ae927994544b
rtzs-cell <rtzs_zheng@outlook.com>
2026-03-14 00:01:52 +0000

    finish

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 # 使用 podman 运行容器（带 GPU 支持）
 
 podman run --rm -it \
-    --device nvidia.com/gpu=1 \
+    --device nvidia.com/gpu=3 \
     --userns=keep-id \
-    -p 8080:8888 \
+    -p 8888:8888 \
     -v "$(pwd)":/app:Z \
     rt-data-gen:latest
